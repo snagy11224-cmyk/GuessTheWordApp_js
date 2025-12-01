ee83275117cbeb516af7bcbad52b003eae7687a6
snagy11224-cmyk <snagy11224@gmail.com>
2025-12-01 18:04:43 +0200

    fixed layout,focus,added keyboard control

--- guess.js
+++ guess.js
@@ -26,7 +26,32 @@ function generateInput() {
     }
     inputsContainer.appendChild(tryDiv);
   }
-  inputsContainer.children[0].children[1].focus;
+  inputsContainer.children[0].children[1].focus();
+  //disable all inputs except first one
+  const inputsInDisabledDiv=document.querySelectorAll(".disabled-inputs input");
+  inputsInDisabledDiv.forEach((input)=>(input.disabled=true));
+  //control focus
+  const inputs=document.querySelectorAll("input")
+  inputs.forEach((input , index)=>{
+    input.addEventListener("input",function(){
+        this.value=this.value.toUpperCase();
+        const nextInput=inputs[index+1];
+        if(nextInput) nextInput.focus();
+    })
+    input.addEventListener("keydown",function(event){
+        const currentIndex=Array.from(inputs).indexOf(event.target);
+        if(event.key==="ArrowRight"){
+            const nextInput=currentIndex+1;
+            if(nextInput<inputs.length) inputs[nextInput].focus();
+        }
+
+        if(event.key==="ArrowLeft"){
+            const prevInput=currentIndex-1;
+            if(prevInput>=0) inputs[prevInput].focus();
+        }
+    })
+
+  });
 }
 
 window.onload = function () {
