e1a01fe931b27e87f3f6bfed7257dc7c4f454e0d
snagy11224-cmyk <snagy11224@gmail.com>
2025-12-01 15:30:48 +0200

    basic js added

---
+++ guess.js
@@ -0,0 +1,4 @@
+let gameName="Guess The Word"
+document.title=gameName;
+document.querySelector("h1").innerHTML=gameName;
+document.querySelector("footer").innerHTML=`${gameName} game Created By Salma`;
