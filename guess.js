b662872f0c628a8d952134d114b8c84a69df73d7
snagy11224-cmyk <snagy11224@gmail.com>
2025-12-01 16:39:00 +0200

    added inputs and trials

--- guess.js
+++ guess.js
@@ -1,4 +1,34 @@
-let gameName="Guess The Word"
-document.title=gameName;
-document.querySelector("h1").innerHTML=gameName;
-document.querySelector("footer").innerHTML=`${gameName} game Created By Salma`;
+let gameName = "Guess The Word";
+document.title = gameName;
+document.querySelector("h1").innerHTML = gameName;
+document.querySelector(
+  "footer"
+).innerHTML = `${gameName} game Created By Salma`;
+
+//setting game options
+let numberOfTries = 6;
+let numberOfLetters = 6;
+let currentTry = 1;
+
+function generateInput() {
+  const inputsContainer = document.querySelector(".inputs");
+  for (let i = 1; i <= numberOfTries; i++) {
+    const tryDiv = document.createElement("div");
+    tryDiv.classList.add(`try-${i}`);
+    tryDiv.innerHTML = `<span>Try ${i}</span>`;
+    if (i !== 1) tryDiv.classList.add("disabled-inputs");
+    for (let j = 1; j <= numberOfLetters; j++) {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = `guess${i}-letter${j}`;
+      input.setAttribute("maxlength", "1");
+      tryDiv.appendChild(input);
+    }
+    inputsContainer.appendChild(tryDiv);
+  }
+  inputsContainer.children[0].children[1].focus;
+}
+
+window.onload = function () {
+  generateInput();
+};
